38c6910b1111ba913c2423e27e759bc5ad269f96
Daniel Magaña <daniel.magana@sansano.usm.cl>
2022-11-11 23:30:00 -0300

    Tabla solicitud

--- front/src/components/Solicitud.js
+++ front/src/components/Solicitud.js
@@ -1,4 +1,5 @@
 import React from "react";
+import solicitud from "../mocking/datos";
 
 class Solicitud extends React.Component{
     render(){
@@ -9,53 +10,31 @@ class Solicitud extends React.Component{
             <table class="table table-bordered table-striped">
                 <thead>
                 <tr>
-                    <th>Firstname</th>
-                    <th>Lastname</th>
-                    <th>Email</th>
-                    <th>Agregar</th>
+                    <th>Título</th>
+                    <th>Tipo</th>
+                    <th>Autor</th>
+                    <th>Categoría</th>
+                    <th>Confirmar</th>
                 </tr>
                 </thead>
                 <tbody id="myTable">
-                <tr>
-                    <td>John</td>
-                    <td>Doe</td>
-                    <td>john@example.com</td>
-                    <td>
-                    <div className="form-check">
-                        <input type="checkbox" className="form-check-input" id="check1" name="option1" value="something" />
-                    </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Mary</td>
-                    <td>Moe</td>
-                    <td>mary@mail.com</td>
-                    <td>
-                    <div className="form-check">
-                        <input type="checkbox" className="form-check-input" id="check2" name="option2" value="something" />
-                    </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>July</td>
-                    <td>Dooley</td>
-                    <td>july@greatstuff.com</td>
-                    <td>
-                    <div className="form-check">
-                        <input type="checkbox" className="form-check-input" id="check3" name="option3" value="something" />
-                    </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Anja</td>
-                    <td>Ravendale</td>
-                    <td>a_r@test.com</td>
-                    <td>
-                    <div className="form-check">
-                        <input type="checkbox" className="form-check-input" id="check4" name="option4" value="something" />
-                    </div>
-                    </td>
-                </tr>
+                    {
+                        solicitud.data.getDocumentos.map((item, index) => {
+                            return (
+                                <tr>
+                                    <td>{item.titulo}</td>
+                                    <td>{item.tipo}</td>
+                                    <td>{item.autor}</td>
+                                    <td>{item.categoria}</td>
+                                    <td>
+                                        <div className="form-check">
+                                            <input type="checkbox" className="form-check-input" id="check1" name="option1" value="something" />
+                                        </div>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
                 </tbody>
             </table>
             <button type="submit" class="btn btn-primary mt-3">Enviar Solicitud</button>
