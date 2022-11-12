8359b295e3216a0cd6e3c53d4c3c9722c12384b9
SebastianCalderonM <74921708+SebastianCalderonM@users.noreply.github.com>
2022-11-11 23:30:44 -0300

    catalogo

--- front/src/components/Catalogo.js
+++ front/src/components/Catalogo.js
@@ -5,6 +5,7 @@ class Catalogo extends React.Component{
     render(){
         return(
             <div class="container">
+            <h2>Catálogo</h2>
             <form action="/action_page.php">
             <table class="table table-bordered table-striped">
                 <thead>
@@ -12,6 +13,7 @@ class Catalogo extends React.Component{
                     <th>Título</th>
                     <th>Tipo</th>
                     <th>Autor</th>
+                    <th>Género</th>
                     <th>Agregar</th>
                 </tr>
                 </thead>
@@ -35,7 +37,8 @@ class Catalogo extends React.Component{
                     }
                 </tbody>
             </table>
-            <button type="submit" class="btn btn-primary mt-3">Submit</button>
+            <button className="btn btn-warning mt-3 px-3">Volver</button>
+            <button type="submit" className="btn btn-primary mt-3">Agregar a Solicitud</button>
             </form>
             </div>
 
